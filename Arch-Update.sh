5fadd80a75994d0b70423e02840afe4b2f950486
yangxins <yangmame@icloud.com>
2017-05-17 08:56:06 +0800

    test

---
+++ Arch-Update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sleep 20
+DECIDE(){
+sleep 30
+while (cat /tmp/arch.log | grep "(*)")
+do notify-send "HEY !" "Your System Need To Upgrade !"
+killall pacaur
+sudo rm -rf /var/lib/pacman/db.lck
+sleep 900
+pacaur -Syu > /tmp/arch.log & DECIDE
+done
+sudo rm -rf /var/lib/pacman/db.lck
+}
+
+while ((1))
+do sudo rm -rf /var/lib/pacman/db.lck
+pacaur -Syu > /tmp/arch.log & DECIDE
+sleep 1800
+done
